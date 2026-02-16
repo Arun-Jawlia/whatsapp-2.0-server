ad2f1024629c458afc320b81fa5ced22af0cd437
Arun-Jawlia <arunkumar08.mk@gmail.com>
2026-02-16 22:23:20 +0530

    fix: accepts friend request services and controllers

--- src/modules/chats/chat.routes.ts
+++ src/modules/chats/chat.routes.ts
@@ -5,7 +5,11 @@ import { chatsController } from "./chat.controllers";
 const router = Router();
 
 router.get("/", requireAuth, chatsController.listChats);
-
+router.get(
+  "/private/:friendId",
+  requireAuth,
+  chatsController.getPrivateChatByFriendId,
+);
 router.get("/:chatId/messages", requireAuth, chatsController.listMessages);
 router.post("/:chatId/messages", requireAuth, chatsController.sendMessage);
 
